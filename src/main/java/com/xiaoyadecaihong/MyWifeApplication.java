eba938dd489222f15b5173079decfd81bc0f87cc
yaguang.wang <wygkehan@gmail.com>
2018-04-24 13:39:20 +0800

    初始化工程

---
+++ src/main/java/com/xiaoyadecaihong/MyWifeApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoyadecaihong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWifeApplication.class, args);
+	}
+}
